5350f878e8efd9fed8da18dc8dfe1efb7729ebcf
kira1317 <smilevskaya@mail.ua>
2015-09-10 15:04:03 +0100

    + interface

--- dao/src/main/java/ContactService.java
+++ dao/src/main/java/ContactService.java
@@ -1,5 +1,26 @@
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
+
 /**
  * Created by Администратор on 10.09.15.
  */
 public interface ContactService {
+
+    ContactDao contactDao = null;
+    HobbyDao hobbyDao = null;
+    PlaseDao placeDao = null;
+    MessagerDao messageDao = null;
+
+    void createContact(String login,String password,LocalDate date);
+    void addHobby(String title, String description);
+    void addPlace(String title, String description, double latitude, double longitude);
+    void addFriendship(Contact contact, Contact contactFriend);
+    Set<Contact> getFriendList(Contact contact);
+    List<Message> getConversation(Contact contact, Contact contactFriend);
+
+
+
+
+
 }
