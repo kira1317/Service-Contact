8aa68e3742da76d63ae18104386a49fb6f26f66b
kira1317 <smilevskaya@mail.ua>
2015-09-30 10:29:33 +0300

    to github

--- dao/src/main/java/ua/service/contact/dao/service/impl/ContactDao.java
+++ dao/src/main/java/ua/service/contact/dao/service/impl/ContactDao.java
@@ -6,9 +6,15 @@ import org.springframework.beans.factory.annotation.Value;
 
 import core.model.Contact;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.annotation.PostConstruct;
 
 /**
  * Created by Администратор on 10.09.15.
@@ -25,6 +31,26 @@ public class ContactDao {
         return serviseContact.values();
     }
 
+
+    @PostConstruct
+    public void init() throws IOException, ClassNotFoundException {
+        serviseContact = new HashMap<Integer, Contact>();
+
+        Contact contact=new Contact(0);
+        contact.setFirstName("Lena");
+        serviseContact.put(contact.getIdContact(),contact);
+
+        File file = new File(fileName);
+        if (file.exists()) {
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream(fileName));
+            serviseContact = (Map<Integer, Contact>)ois.readObject();
+            ois.close();
+        }
+        else {
+            System.out.println("File was not found");
+        }
+
+    }
 
 
     public boolean addContact(Contact contact){
